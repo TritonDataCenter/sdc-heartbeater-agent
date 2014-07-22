dcad105228cff2fbf4091d7eeef075522eeb0e93
Orlando Vazquez <ovazquez@gmail.com>
2014-07-22 08:58:42 +0000

    AGENT-730: start disabled based on feature flag

--- npm/postinstall.sh
+++ npm/postinstall.sh
@@ -3,16 +3,26 @@
 set -o xtrace
 DIR=`dirname $0`
 
+. /lib/sdc/config.sh
+
+load_sdc_config
+
 export PREFIX=$npm_config_prefix
 export ETC_DIR=$npm_config_etc
 export SMF_DIR=$npm_config_smfdir
 export VERSION=$npm_package_version
+export ENABLED=true
+
+if [[ $CONFIG_die_rabbit_die == "true" ]]; then
+    export ENABLED=false
+fi
 
 subfile () {
   IN=$1
   OUT=$2
   sed -e "s#@@PREFIX@@#$PREFIX#g" \
       -e "s/@@VERSION@@/$VERSION/g" \
+      -e "s/@@ENABLED@@/$ENABLED/g" \
       $IN > $OUT
 }
 
